c7a2bdd4cab73a9bacdfd72ec0c452403cde0dda
Daniel <tvland140@gmail.com>
2025-07-28 18:25:44 -0500

    Persona atributos y Constructor

--- INDIVIDUOS/Persona.java
+++ INDIVIDUOS/Persona.java
@@ -1,4 +1,22 @@
 package INDIVIDUOS;
 
+//Persona es la clase padre de Niño y Madre
 public class Persona {
+
+//Definimos atributos de la clase persona como PROTECTED
+    protected String nombres;
+    protected String apellidos;
+    protected String cedula;
+    protected String sexo;
+
+    //Creamos un CONSTRUCTOR
+    public Persona(String nombres, String apellidos, String cedula, String sexo) {
+        this.nombres=nombres;
+        this.apellidos=apellidos;
+        this.cedula=cedula;
+        this.sexo=sexo;
+    }
+
+
+
 }
